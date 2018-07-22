5ba8cbe07b49649e943684e4b11d0df7174a1a19
Alex Magar <lx.mgr07@gmail.com>
2018-07-22 18:13:21 +0545

    execlp() implemented with comments

--- execlp.c
+++ execlp.c
@@ -2,19 +2,29 @@
 #include <unistd.h>
 
 int main(){
-	
+
 	int pd[2]; //pipe descriptor
 
 	pipe(pd);
 
 	if(fork() == 0){
-		close(1);
+		//close() file descriptor, so that it no longer refers to any file and may be reused
+
+		close(1); //stop writing
+
+		//dup(int oldfd);
+		//dup() is a system call that allow us to change file descriptor
+		//dup2(int oldfd, int newfd);
+		//dup3(int oldfd, int newfd, int flags)
+		//dup(pd[1]) creates a copy of the pipe descriptor
 		dup(pd[1]); //the output is now sent to pipe
+
 		close(pd[0]); // we are not going to read from pipe
 		execlp("ls","ls","-l",NULL);
 		//child
 	}else{
-		close(0);
+		close(0); //stop reading
+		
 		dup(pd[0]); //the input in now recvd from pipe
 		close(pd[1]); //we are not going to write to the pipe
 		//parent
